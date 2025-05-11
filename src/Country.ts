f3fed4fccca73330e1d9bf294721df095270839e
Arnd Rosemeier <git@futuremagic.de>
2025-05-11 16:25:32 +0200

    feat: add income property to Country and CountryKnowledge, and money property to Player; expand Player knowledge system

--- src/Country.ts
+++ src/Country.ts
@@ -4,15 +4,17 @@ export class Country {
   name: string;
   owner: Player | null;
   armies: number;
+  income: number;
   coordinates: [number, number][];
   border: [number, number][];
   oceanBorder: [number, number][];
   neighbors: Country[];
 
-  constructor(name: string, owner: Player | null = null, armies: number = 0) {
+  constructor(name: string, owner: Player | null = null, armies: number = 0, income: number = 0) {
     this.name = name;
     this.owner = owner;
     this.armies = armies;
+    this.income = income;
     this.coordinates = [];
     this.border = [];
     this.oceanBorder = [];
--- src/Player.ts
+++ src/Player.ts
@@ -1,9 +1,41 @@
+import { Country } from './Country';
+
+export interface CountryKnowledge {
+  country: Country;
+  gameTurn: number;
+  army: number; // observed army size at time of spying
+  income: number; // observed income at time of spying
+}
+
 export class Player {
   name: string;
   color: string;
+  ownedCountries: Country[];
+  homeCountry: Country | null;
+  /**
+   * Player's available money.
+   */
+  money: number;
+  /**
+   * Knowledge about countries the player owns or has spied upon.
+   * Each entry is a country and the turn it was spied upon.
+   */
+  knowledge: CountryKnowledge[];
 
-  constructor(name: string, color: string) {
+  constructor(
+    name: string,
+    color: string,
+    ownedCountries: Country[] = [],
+    homeCountry: Country | null = null,
+    knowledge: CountryKnowledge[] = [],
+    money: number = 0
+  ) {
     this.name = name;
     this.color = color;
+    this.ownedCountries = ownedCountries;
+    this.homeCountry = homeCountry;
+    this.knowledge = knowledge;
+    this.money = money;
   }
 }
+
